42c738694d0496c2f75a3b49959c193e43fbcb59
mgs08 <matthewsteed08@yahoo.com>
2023-07-13 14:20:50 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const apikey = "6aec565cee723d091c7e00bd2b0da758";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const citySearchEl = document.getElementById("city-search");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit" , (event)=>{
+    event.preventDefault();
+    const cityValue = citySearchEl.value;
+    getWeatherData(cityValue)
+}
+);
+
+async function getWeatherData(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`);
+    
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        };
+
+        const data = await response.json();
+        const temperature = Math.round(data.main.temp)
+
+        const description = data.weather[0].description
+
+        const icon = data.weather[0].icon
+
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${Math.round(data.main.humidity)}%`,
+            `Wind Speed: ${Math.round(data.wind.speed)}m/s`,
+        ];
+
+        weatherDataEl.querySelector(".icon").innerHTML = `<img
+        src="http://openweathermap.org/img/wn/${icon}.png"
+        alt="Weather Icon"/>`;
+        weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`;
+        weatherDataEl.querySelector(".description").textContent = `${description}`;
+        weatherDataEl.querySelector(".details").innerHTML = details.map((detail)=>
+        `<div>${detail}</div>`).join("");
+    } catch (error) {
+        weatherDataEl.querySelector(".icon").innerHTML = "";
+        weatherDataEl.querySelector(".temperature").textContent = "";
+        weatherDataEl.querySelector(".description").textContent = "An error happened. Please try again later";
+        weatherDataEl.querySelector(".details").innerHTML = "";
+    };
+};
